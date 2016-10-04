67e88b8eb263055b575e66eb1ec8a495de1f5c05
Samuel Pattuzzi <sam@pattuzzi.it>
2016-10-04 13:23:21 +0100

    Setup a VR Camera

---
+++ Assets/Plugins/iOS/CardboardAppController.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff24c325e9bd5a3a452ec55107096ee087dbddcd0e0962eec3962ef28087d806
+size 1139
---
+++ Assets/Plugins/iOS/CardboardAppController.mm
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2619b4749db936870b4ecece4efb1889ac14281b508e0622565dc509c4b93b90
+size 2213
