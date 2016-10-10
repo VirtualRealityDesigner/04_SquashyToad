9113366f8a61f9f86b0c9708408a6f9029ec00fe
Samuel Pattuzzi <sam@pattuzzi.it>
2016-10-10 16:09:59 +0100

    Avoiding Motion Sickness

---
+++ Assets/FrogMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrogMovement : MonoBehaviour {
+
+	public Vector3 jumpVector;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange);
+		}
+	}
+}
