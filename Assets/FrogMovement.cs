0e51e56c7dac58636208939f1a6d73e909882d7e
Samuel Pattuzzi <sam@pattuzzi.it>
2016-10-27 17:33:08 +0100

    Debugging Vectors in Unity

--- Assets/FrogMovement.cs
+++ Assets/FrogMovement.cs
@@ -12,7 +12,13 @@ public class FrogMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space))
+        Debug.DrawRay(transform.position, jumpVector, Color.blue);
+        var projectedJumpVector = Vector3.ProjectOnPlane(jumpVector, Vector3.up);
+        Debug.DrawRay(transform.position, projectedJumpVector, Color.blue);
+        var radiansToRotate = Mathf.Deg2Rad * 90;
+        var rotatedJumpVector = Vector3.RotateTowards(projectedJumpVector, Vector3.up, radiansToRotate, 0);
+        Debug.DrawRay(transform.position, rotatedJumpVector.normalized, Color.blue);
+        if (Input.GetKeyDown(KeyCode.Space))
 		{
 			GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange);
 		}
