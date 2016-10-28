4464bb50e7aa7188a1940998d658baab39d66d1a
Samuel Pattuzzi <sam@pattuzzi.it>
2016-10-28 10:51:49 +0100

    Advanced Vector Manipulation

--- Assets/FrogMovement.cs
+++ Assets/FrogMovement.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class FrogMovement : MonoBehaviour {
 
-	public Vector3 jumpVector;
+    public float jumpElevationInDegrees = 45;
+    public float jumpSpeedInMPS = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -12,12 +13,14 @@ public class FrogMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        var camera = GetComponentInChildren<Camera>();
+        Debug.DrawRay(transform.position, camera.transform.forward, Color.blue);
+        var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
+        Debug.DrawRay(transform.position, projectedLookDirection, Color.blue);
+        var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
+        var unnormalizedJumpDirection = Vector3.RotateTowards(projectedLookDirection, Vector3.up, radiansToRotate, 0);
+        var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInMPS;
         Debug.DrawRay(transform.position, jumpVector, Color.blue);
-        var projectedJumpVector = Vector3.ProjectOnPlane(jumpVector, Vector3.up);
-        Debug.DrawRay(transform.position, projectedJumpVector, Color.blue);
-        var radiansToRotate = Mathf.Deg2Rad * 90;
-        var rotatedJumpVector = Vector3.RotateTowards(projectedJumpVector, Vector3.up, radiansToRotate, 0);
-        Debug.DrawRay(transform.position, rotatedJumpVector.normalized, Color.blue);
         if (Input.GetKeyDown(KeyCode.Space))
 		{
 			GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange);
