60e73609961ef3276b3b16d498b2f145f78b2833
Samuel Pattuzzi <sam@pattuzzi.it>
2016-11-03 15:43:41 +0000

    Jumping Only When Grounded

--- Assets/FrogMovement.cs
+++ Assets/FrogMovement.cs
@@ -5,6 +5,8 @@ public class FrogMovement : MonoBehaviour {
 
     public float jumpElevationInDegrees = 45;
     public float jumpSpeedInMPS = 5;
+    public float jumpGroundClearance = 2;
+    public float jumpSpeedTolerance = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -13,17 +15,18 @@ public class FrogMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        var camera = GetComponentInChildren<Camera>();
-        Debug.DrawRay(transform.position, camera.transform.forward, Color.blue);
-        var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
-        Debug.DrawRay(transform.position, projectedLookDirection, Color.blue);
-        var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
-        var unnormalizedJumpDirection = Vector3.RotateTowards(projectedLookDirection, Vector3.up, radiansToRotate, 0);
-        var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInMPS;
-        Debug.DrawRay(transform.position, jumpVector, Color.blue);
-        if (Input.GetKeyDown(KeyCode.Space))
+        bool isOnGround = Physics.Raycast(transform.position, -transform.up, jumpGroundClearance);
+        Debug.DrawRay(transform.position, -transform.up * jumpGroundClearance);
+        var speed = GetComponent<Rigidbody>().velocity.magnitude;
+        bool isNearStationary = speed < jumpSpeedTolerance; 
+        if (GvrViewer.Instance.Triggered && isOnGround && isNearStationary)
 		{
-			GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange);
+            var camera = GetComponentInChildren<Camera>();
+            var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
+            var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
+            var unnormalizedJumpDirection = Vector3.RotateTowards(projectedLookDirection, Vector3.up, radiansToRotate, 0);
+            var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInMPS;
+            GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange);
 		}
 	}
 }
---
+++ readme.md
@@ -0,0 +1,12 @@
+1. Becoming an Android Developer
+1. Building an Unity Android Project
+1. Becoming an iOS Developer
+1. Building an Unity iOS Project
+1. Setup a VR Camera
+1. UI in VR Environments
+1. Interacting with Buttons
+1. Google VR Spatial Audio Sources
+1. Avoiding Motion Sickness
+1. Debugging Vectors in Unity
+1. Advanced Vector Manipulation
+1. Jumping Only When Grounded
