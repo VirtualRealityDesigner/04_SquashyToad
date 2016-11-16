96234715b6df8e3eb11d358b8c09a3c276351693
Samuel Pattuzzi <sam@pattuzzi.it>
2016-11-16 11:30:49 +0000

    Poisson interarrival cars

---
+++ Assets/CurveSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurveSpawner : MonoBehaviour {
+
+    public float rate = 4;
+    public float minimumGap = 2;
+    public GameObject prefab;
+    float nextArrivalTime = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(nextArrivalTime < Time.time)
+        {
+            Spawn();
+            var interval = NextArrivalInterval();
+            print(interval);
+            nextArrivalTime = Time.time + interval;
+        }
+	}
+
+    void Spawn()
+    {
+        var instance = Instantiate(prefab, transform.position, transform.rotation, transform);
+    }
+
+    float NextArrivalInterval()
+    {
+        return -Mathf.Log(Random.value) * rate + minimumGap;
+    }
+}
---
+++ Assets/VehicleMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleMovement : MonoBehaviour {
+
+    public float velocity = 500;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, velocity * Time.deltaTime);
+	}
+}
