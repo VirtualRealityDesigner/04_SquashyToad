e09ea1980158a023ef7713991c4f32b609ed583f
Samuel Pattuzzi <sam@pattuzzi.it>
2016-11-16 13:15:42 +0000

    ST19 Moving Cars with Kinematic Colliders

---
+++ Assets/VehicleMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleMovement : MonoBehaviour {
+
+    float velocity = 1000;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-velocity * Time.deltaTime, 0, 0);
+	}
+}
