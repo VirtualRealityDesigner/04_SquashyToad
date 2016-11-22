55ca618158bb72f6b6b7117624b07c7d5c653d3a
Samuel Pattuzzi <sam@pattuzzi.it>
2016-11-22 11:28:59 +0000

    Add multiple jumping levels for being able to more finely control movement.

--- Assets/FrogMovement.cs
+++ Assets/FrogMovement.cs
@@ -4,29 +4,44 @@ using System.Collections;
 public class FrogMovement : MonoBehaviour {
 
     public float jumpElevationInDegrees = 45;
-    public float jumpSpeedInCMPS = 5;
+    public float[] jumpSpeeds = { 200, 300, 400 };
     public float jumpGroundClearance = 2;
-    public float jumpSpeedTolerance = 5;
+    public int jumpCount = 0;
+    public int collisionCount = 0;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+    void OnCollisionEnter ()
+    {
+        collisionCount++;
+    }
+
+    void OnCollisionExit ()
+    {
+        collisionCount--;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        bool isOnGround = Physics.Raycast(transform.position, -transform.up, jumpGroundClearance);
+        print(jumpCount);
+
+        if (collisionCount > 0)
+        {
+            jumpCount = 0;
+        }
         Debug.DrawRay(transform.position, -transform.up * jumpGroundClearance);
-        var speed = GetComponent<Rigidbody>().velocity.magnitude;
-        bool isNearStationary = speed < jumpSpeedTolerance; 
-        if (GvrViewer.Instance.Triggered && isOnGround && isNearStationary)
+        if (GvrViewer.Instance.Triggered && jumpSpeeds.Length > jumpCount)
 		{
             var camera = GetComponentInChildren<Camera>();
             var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
             var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
             var unnormalizedJumpDirection = Vector3.RotateTowards(projectedLookDirection, Vector3.up, radiansToRotate, 0);
-            var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInCMPS;
+            var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeeds[jumpCount];
             GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange);
-		}
-	}
+            jumpCount++;
+        }
+    }
 }
