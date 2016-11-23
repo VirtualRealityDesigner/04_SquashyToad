92a5875bb1932d509d19ec316811a76afc8187c8
Samuel Pattuzzi <sam@pattuzzi.it>
2016-11-23 13:40:15 +0000

    ST25 Double and Triple Hop Jumping

--- Assets/FrogMovement.cs
+++ Assets/FrogMovement.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class FrogMovement : MonoBehaviour {
 
     public float jumpElevationInDegrees = 45;
-    public float jumpSpeedInCMPS = 5;
+    public float[] jumpSpeedInCMPS = { 200, 400, 700 };
     public float jumpGroundClearance = 2;
     public float jumpSpeedTolerance = 5;
 
     public int collisionCount = 0;
+    public int hopCount = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -28,13 +29,19 @@ public class FrogMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         bool isOnGround = collisionCount > 0;
-        if (GvrViewer.Instance.Triggered && isOnGround)
+
+        if (isOnGround)
+        {
+            hopCount = 0;
+        }
+
+        if (GvrViewer.Instance.Triggered && hopCount < jumpSpeedInCMPS.Length)
 		{
             var camera = GetComponentInChildren<Camera>();
             var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
             var radiansToRotate = Mathf.Deg2Rad * jumpElevationInDegrees;
             var unnormalizedJumpDirection = Vector3.RotateTowards(projectedLookDirection, Vector3.up, radiansToRotate, 0);
-            var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInCMPS;
+            var jumpVector = unnormalizedJumpDirection.normalized * jumpSpeedInCMPS[hopCount];
             GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange);
 		}
 	}
