defe3dd099fb0d20a4c0c6a30d7b7db84516b7f8
Samuel Pattuzzi <sam@pattuzzi.it>
2016-12-12 17:35:13 +0000

    ST28 HUD UI and Level Reload

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -4,13 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-	public void LoadNextScene()
-	{
-		int currentIndex = SceneManager.GetActiveScene().buildIndex;
-		SceneManager.LoadScene(currentIndex + 1);
-	}
+    public void LoadNextScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
+    }
 
-	public void Quit()
+    public void ReloadScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex);
+    }
+
+    public void Quit()
 	{
 		print("Game will quit.");
 		Application.Quit();
