82c7c1b2a928766239f3a7743cb844c8a12db72a
Samuel Pattuzzi <sam@pattuzzi.it>
2016-12-15 15:27:39 +0000

    Add froggy reticule.

--- Assets/Death.cs
+++ Assets/Death.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class Death : MonoBehaviour {
 
     public GameObject UICanvas;
+    public GameObject Reticule;
 
     public void OnDeath()
     {
         UICanvas.SetActive(true);
+        Reticule.SetActive(true);
         GetComponent<Rigidbody>().isKinematic = true;
 
     }
