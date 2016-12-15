145eea2ea87f20d7cb73e5abd6184cabf0754eee
Samuel Pattuzzi <sam@pattuzzi.it>
2016-12-15 15:05:28 +0000

    Adjust shadows properly with editor script.

---
+++ Assets/LightSetter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LightSetter : MonoBehaviour {
+
+    public float shadowBias = 0;
+
+	// Use this for initialization
+	void Update () {
+        GetComponent<Light>().shadowBias = shadowBias;
+	}
+	
+}
