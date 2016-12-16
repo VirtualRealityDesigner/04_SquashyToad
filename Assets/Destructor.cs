145f92ed5a6df8587cd875404622339623e135c7
Samuel Pattuzzi <sam@pattuzzi.it>
2016-12-16 13:01:21 +0000

    ST31 Garbage Collecting GameObjects

---
+++ Assets/Destructor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructor : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
