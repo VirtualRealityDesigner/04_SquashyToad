b304e22fdc46022b14a061537879190a381184bf
Samuel Pattuzzi <sam@pattuzzi.it>
2016-12-22 10:41:33 +0000

    ST34 Basic Performance Benchmarking

--- Assets/LaneSpawner.cs
+++ Assets/LaneSpawner.cs
@@ -9,11 +9,12 @@ public class LaneSpawner : MonoBehaviour {
     public GameObject[] dangerousLanePrefabs;
     LaneType lastLaneType = LaneType.Safe;
     public float safeLaneRunProbability = 0.2f;
+    public float laneSpawnDistance = 50000;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         int offset = 0;
-        while (offset < 50000)
+        while (offset < laneSpawnDistance)
         {
             CreateRandomLane(offset);
             offset += 1000;
